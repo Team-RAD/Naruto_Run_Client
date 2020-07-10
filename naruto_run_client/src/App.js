953bd7c85a8fd97ea0e04a528f2014c41cc9f4b2
Insidium <ryanchallen@outlook.com>
2020-07-11 09:21:45 +1000

    Removed boilerplate comments on index.html and added bootstrap anf font awesome CDN urls

--- naruto_run_client/src/App.js
+++ naruto_run_client/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Hey Dave!</h1>
+		</div>
+	);
+};
 
-export default App
+export default App;
