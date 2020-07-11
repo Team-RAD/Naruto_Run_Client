cce6d4fb6280f204e6e2e8e69330ad18dd6fb42c
Insidium <ryanchallen@outlook.com>
2020-07-11 12:43:50 +1000

    installed axios and classnames, changed about icon, created default about component,added swtich and route and imported to app

--- naruto_run_client/src/App.js
+++ naruto_run_client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import AppNavBar from './components/layout/AppNavBar';
+import About from './components/pages/About';
 
 const App = () => {
 	return (
-		<div>
+		<div className='App'>
 			<Router>
 				<AppNavBar branding='Naruto Run' />
 				<div className='container'>
-					<h1>Naruto Run</h1>
-					<p>Run headfirst at all the things... faster</p>
+					<Switch>
+						<Route exact path='/about' component={About}></Route>
+					</Switch>
 				</div>
 			</Router>
 		</div>
---
+++ naruto_run_client/src/components/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<h1 className='display-4'>What is Naruto Run?</h1>
+			<p className='lead'>
+				Naruto Run helps to speed up the process of identifying which area of
+				the tech world people want to work in and the best road to run down to
+				get there. It does this by providing a hub for the posting of tech
+				career journeys by users already in the industry (contributors) for the
+				reference of other users (seekers) wanting to make the move into tech.
+				It allows contributors to outline their path into a tech career in a
+				condensed, efficient form, so that seekers can quickly distill this
+				information and that of other NR posts into a clearer picture of where
+				they should focus their efforts.
+			</p>
+			<p className='text-secondary'>Version 1.0.0</p>
+		</div>
+	);
+};
