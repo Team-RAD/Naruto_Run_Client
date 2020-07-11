7451505332c165576fd1cb50ee4d18e487bcf7ac
Insidium <ryanchallen@outlook.com>
2020-07-11 11:30:53 +1000

    Installed react-router-dom for initial routing on client, created navbar with initial links, imported into app

--- naruto_run_client/src/App.js
+++ naruto_run_client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavBar from './components/layout/AppNavBar';
 
 const App = () => {
 	return (
 		<div>
-			<h1>Hey Dave!</h1>
+			<Router>
+				<AppNavBar branding='Naruto Run' />
+				<div className='container'>
+					<h1>Naruto Run</h1>
+					<p>Run headfirst at all the things... faster</p>
+				</div>
+			</Router>
 		</div>
 	);
 };
