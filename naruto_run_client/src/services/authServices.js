d70e6e2de9b8d09f440067cb394fb9dc3ef0ef05
Insidium <ryanchallen@outlook.com>
2020-07-17 10:14:55 +1000

    Created api file for axios instance, added store for context and global state, added login logout register and authenticated functions added loginUser functionality to Login form onSubmit

---
+++ naruto_run_client/src/services/authServices.js
@@ -0,0 +1,43 @@
+import api from '../config/api';
+
+//post user details to login auth route and return response confirming retrieval
+export async function loginUser(userInfo) {
+	const response = await api.post('/auth/login', userInfo);
+	console.log('Retreived user from server', response);
+	return response.data;
+}
+
+//return get user details to logout auth route
+export async function logoutUser() {
+	return api.get('/auth/logout');
+}
+
+//post user details to register auth route and return response confirming retrieval
+export async function registerUser(userInfo) {
+	const response = await api.post('/auth/register', userInfo);
+	console.log('Retreived user from server', response);
+	return response.data;
+}
+
+//get response checking if user is authenticated via user auth route, throw an error if not
+export async function userAuthenticated() {
+	try {
+		const response = await api.get('/auth/user');
+		return response;
+	} catch (error) {
+		console.log('An error occurred while authenticating user');
+		throw error;
+	}
+}
+
+//get the logged in user from localStorage
+export function getLoggedInUser() {
+	return loggedInUser.getItem('loggedInUser');
+}
+
+//store the logged in user to localStorage
+export function setLoggedInUser(user) {
+	user
+		? localStorage.setItem('loggedInUser', user)
+		: localStorage.removeItem('loggedInUser');
+}
