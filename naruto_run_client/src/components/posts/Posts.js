77e22c20ae97f875415fe70fdb25cd5a7c90395a
Insidium <ryanchallen@outlook.com>
2020-07-18 12:10:30 +1000

    Installed TimeAgo for modified date use, added function for fetching all posts to app, created post structure with link to unique post via username heading, added logic to posts to iterate over each and display, created getAllPosts and delete functions for posts in services

--- naruto_run_client/src/components/posts/Posts.js
+++ naruto_run_client/src/components/posts/Posts.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import NarutoPost from './Post';
+import { useGlobalState } from '../../config/store';
 
-class Posts extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Here be the posts!</h1>
-			</div>
-		);
-	}
-}
+const Posts = () => {
+	const { store } = useGlobalState();
+	const { narutoPosts } = store;
+	return (
+		<div>
+			{narutoPosts
+				.sort((a, b) => b.modified_date - a.modified_date)
+				.map((post) => (
+					<NarutoPost key={post._id} post={post} />
+				))}
+		</div>
+	);
+};
 
 export default Posts;
