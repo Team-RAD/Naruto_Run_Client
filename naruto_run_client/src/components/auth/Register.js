40337fd8c11b9d3319bf1477e7f2e6d92af4535e
Insidium <ryanchallen@outlook.com>
2020-07-18 14:15:37 +1000

    Changed order of nav links for logged in user, added register form functionality

--- naruto_run_client/src/components/auth/Register.js
+++ naruto_run_client/src/components/auth/Register.js
@@ -1,89 +1,112 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useGlobalState } from '../../config/store';
+import { registerUser } from '../../services/authServices';
 
-class Register extends Component {
-	state = {
+const Register = ({ history }) => {
+	const initialFormState = {
+		username: '',
 		email: '',
-		password: '',
-		firstName: '',
-		lastName: ''
+		password: ''
 	};
 
-	onSubmit = (e) => {
+	const [userDetails, setUserDetails] = useState(initialFormState);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const { dispatch } = useGlobalState();
+
+	function onChange(e) {
+		const name = e.target.name;
+		const value = e.target.value;
+		setUserDetails({
+			...userDetails,
+			[name]: value
+		});
+	}
+
+	function onSubmit(e) {
 		e.preventDefault();
-		// const { email, password } = this.state;
-	};
+		registerUser(userDetails)
+			.then(() => {
+				dispatch({ type: 'setLoggedInUser', data: userDetails.username });
+				history.push('/');
+			})
+			.catch((error) => {
+				const status = error.response ? error.response.status : 500;
+				if (status === 409) {
+					setErrorMessage(
+						'This user already exists. Please login or specify a valid username'
+					);
+				} else {
+					setErrorMessage(
+						'There was an unexpected issue with the server. Please wait a moment and try again.'
+					);
+				}
+				console.log(
+					`Registration failed with error ${error} and status ${status}`
+				);
+			});
+	}
 
-	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-	render() {
-		return (
-			<div className='row'>
-				<div className='col-md-6 mx-auto'>
-					<div className='card'>
-						<div className='card-body'>
-							<h1 className='text-center pb-4 pt-3'>
-								<span className='text-warning'>
-									<i className='fas fa-file-alt'></i>
-								</span>{' '}
-								Register{' '}
-							</h1>
-							<form onSubmit={this.onSubmit}>
-								<div className='form-group'>
-									<label htmlFor='firstName'>First Name</label>
-									<input
-										type='text'
-										className='form-control'
-										name='firstName'
-										required
-										value={this.state.firstName}
-										onChange={this.onChange}
-									/>
+	return (
+		<div className='row'>
+			<div className='col-md-6 mx-auto'>
+				<div className='card'>
+					<div className='card-body'>
+						<h1 className='text-center pb-4 pt-3'>
+							<span className='text-warning'>
+								<i className='fas fa-file-alt'></i>
+							</span>{' '}
+							Register{' '}
+						</h1>
+						<form onSubmit={onSubmit}>
+							{errorMessage && (
+								<div className='alert alert-danger text-center' role='alert'>
+									{errorMessage}
 								</div>
-								<div className='form-group'>
-									<label htmlFor='lastName'>Last Name</label>
-									<input
-										type='text'
-										className='form-control'
-										name='lastName'
-										required
-										value={this.state.lastName}
-										onChange={this.onChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='email'>Email</label>
-									<input
-										type='text'
-										className='form-control'
-										name='email'
-										required
-										value={this.state.email}
-										onChange={this.onChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='password'>Password</label>
-									<input
-										type='password'
-										className='form-control'
-										name='password'
-										required
-										value={this.state.password}
-										onChange={this.onChange}
-									/>
-								</div>
+							)}
+							<div className='form-group'>
+								<label htmlFor='username'>Name</label>
 								<input
-									type='submit'
-									value='Register'
-									className='btn btn-warning btn-block'
+									type='text'
+									className='form-control'
+									name='username'
+									placeholder='Enter your username...'
+									required
+									onChange={onChange}
 								/>
-							</form>
-						</div>
+							</div>
+							<div className='form-group'>
+								<label htmlFor='email'>Email</label>
+								<input
+									type='email'
+									className='form-control'
+									name='email'
+									placeholder='Enter your email...'
+									required
+									onChange={onChange}
+								/>
+							</div>
+							<div className='form-group'>
+								<label htmlFor='password'>Password</label>
+								<input
+									type='password'
+									className='form-control'
+									name='password'
+									placeholder='Enter your password...'
+									required
+									onChange={onChange}
+								/>
+							</div>
+							<input
+								type='submit'
+								value='Register'
+								className='btn btn-warning btn-block'
+							/>
+						</form>
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Register;
