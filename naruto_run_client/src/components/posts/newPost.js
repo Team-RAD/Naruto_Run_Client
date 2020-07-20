b77ce0674b082134a0e2e2307956ae3a4f1e1fe5
Insidium <ryanchallen@outlook.com>
2020-07-20 15:04:47 +1000

    Attempted to render NewPost component, still not working, will seek assistance

--- naruto_run_client/src/components/posts/newPost.js
+++ naruto_run_client/src/components/posts/newPost.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { useGlobalState } from '../../config/store';
 import { addNarutoPost } from '../../services/narutoPostServices';
 
-const NewNarutoPost = ({ history }) => {
+const NewPost = ({ history }) => {
 	function onChange(e) {
 		const name = e.target.name;
 		const value = e.target.value;
@@ -69,71 +69,71 @@ const NewNarutoPost = ({ history }) => {
 		favourite_coding_playlist: '',
 		follow_me_links: ''
 	};
+
 	const [formState, setFormState] = useState(initialFormState);
 	const [errorMessage, setErrorMessage] = useState(null);
 	const { store, dispatch } = useGlobalState();
 	const { narutoPosts } = store;
 
 	return (
-		<h1>Hello</h1>
-		// <form id='newPostForm' onSubmit={onSubmit}>
-		// 	{errorMessage && (
-		// 		<div className='alert alert-danger text-center' role='alert'>
-		// 			{errorMessage}
-		// 		</div>
-		// 	)}
-		// 	<div>
-		// 		<label>Pre Tech Job</label>
-		// 		<input
-		// 			type='text'
-		// 			name='pre_tech_job'
-		// 			placeholder='Enter your job prior to tech'
-		// 			onChange={onChange}
-		// 		/>
-		// 	</div>
-		// 	<div>
-		// 		<label>Current Tech Job</label>
-		// 		<input
-		// 			type='text'
-		// 			name='current_tech_job'
-		// 			placeholder='Enter your current job in tech'
-		// 			onChange={onChange}
-		// 		/>
-		// 	</div>
-		// 	<div>
-		// 		<label>Education Route</label>
-		// 		<input
-		// 			type='text'
-		// 			name='education'
-		// 			placeholder='Enter the education route you took'
-		// 			onChange={onChange}
-		// 		/>
-		// 	</div>
-		// 	<div>
-		// 		<label>Resources You Needed</label>
-		// 		<input
-		// 			type='text'
-		// 			name='resources_required'
-		// 			placeholder='Enter the resources you needed'
-		// 			onChange={onChange}
-		// 		/>
-		// 	</div>
-		// 	<div>
-		// 		<label>Time Taken</label>
-		// 		<input
-		// 			type='text'
-		// 			name='time_taken'
-		// 			placeholder='Enter the approx time taken'
-		// 			onChange={onChange}
-		// 		/>
-		// 	</div>
-		// 	<input
-		// 		type='submit'
-		// 		value='Submit'
-		// 		className='btn btn-warning btn-block'
-		// 	/>
-		// </form>
+		<form id='newPostForm' onSubmit={onSubmit}>
+			{errorMessage && (
+				<div className='alert alert-danger text-center' role='alert'>
+					{errorMessage}
+				</div>
+			)}
+			<div>
+				<label>Pre Tech Job</label>
+				<input
+					type='text'
+					name='pre_tech_job'
+					placeholder='Enter your job prior to tech'
+					onChange={onChange}
+				/>
+			</div>
+			<div>
+				<label>Current Tech Job</label>
+				<input
+					type='text'
+					name='current_tech_job'
+					placeholder='Enter your current job in tech'
+					onChange={onChange}
+				/>
+			</div>
+			<div>
+				<label>Education Route</label>
+				<input
+					type='text'
+					name='education'
+					placeholder='Enter the education route you took'
+					onChange={onChange}
+				/>
+			</div>
+			<div>
+				<label>Resources You Needed</label>
+				<input
+					type='text'
+					name='resources_required'
+					placeholder='Enter the resources you needed'
+					onChange={onChange}
+				/>
+			</div>
+			<div>
+				<label>Time Taken</label>
+				<input
+					type='text'
+					name='time_taken'
+					placeholder='Enter the approx time taken'
+					onChange={onChange}
+				/>
+			</div>
+			<input
+				type='submit'
+				value='Submit'
+				className='btn btn-warning btn-block'
+			/>
+		</form>
 	);
 };
 
-export default NewNarutoPost;
+export default withRouter(NewPost);
