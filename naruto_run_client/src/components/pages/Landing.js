8532e7b989c912173879f1b712a63af751e0bc0b
Insidium <ryanchallen@outlook.com>
2020-07-23 15:32:46 +1000

    Added new logo icon to favicon spot, replaced old favicon, created landing page initial layout, added larger logo for landing page, changed routes to make landing on root route and posts on Posts

---
+++ naruto_run_client/src/components/pages/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<div>
+				<h1 className='display-3 text-center'>
+					Welcome to <span className='text-warning'>Naruto Run</span>!
+				</h1>
+				<div>
+					<img src='../../NR_logo.svg' alt='Naruto Run Logo' />
+				</div>
+				<p className='text-secondary text-center'>Run head first into Tech!</p>
+			</div>
+		</div>
+	);
+};
