1560a81763d00d1b7cc523266b255510f91ef55f
Insidium <ryanchallen@outlook.com>
2020-07-24 15:02:15 +1000

    Added authenticated user logic to App, changed display on posts to only show two fields, the rest on post/:id

--- naruto_run_client/src/App.js
+++ naruto_run_client/src/App.js
@@ -2,13 +2,18 @@ import React, { useReducer, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import stateReducer from './config/stateReducer';
 import { StateContext } from './config/store';
-import { getAllNarutoPosts } from './services/narutoPostServices';
+import { getAllNarutoPosts, getPostById } from './services/narutoPostServices';
+import {
+	userAuthenticated,
+	setLoggedInUser,
+	getLoggedInUser
+} from './services/authServices';
 import AppNavBar from './components/layout/AppNavBar';
 import Landing from './components/pages/Landing';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Posts from './components/posts/Posts';
-import ViewPost from './components/posts/ViewPost';
+import NarutoPost from './components/posts/Post';
 import NewPost from './components/posts/NewPost';
 import EditPost from './components/posts/EditPost';
 import Login from './components/auth/Login';
@@ -17,11 +22,8 @@ import Register from './components/auth/Register';
 const App = () => {
 	const initialState = {
 		loggedInUser: null,
-		narutoPosts: [],
-		error: null
+		narutoPosts: []
 	};
-
-	const [store, dispatch] = useReducer(stateReducer, initialState);
 
 	function fetchNarutoPosts() {
 		getAllNarutoPosts()
@@ -29,15 +31,39 @@ const App = () => {
 				dispatch({ type: 'setNarutoPosts', data: narutoData });
 			})
 			.catch((error) => {
-				console.log(
-					'Error occurred while fetching Naruto Posts from server',
-					error
-				);
+				dispatch({
+					type: 'setError',
+					data: true
+				});
+				console.log('Error occurred while fetching Naruto Posts from server');
 			});
 	}
 
+	const [store, dispatch] = useReducer(stateReducer, initialState);
+	const { narutoPosts, error } = store;
+
 	useEffect(() => {
 		fetchNarutoPosts();
+		userAuthenticated()
+			.then(() => {
+				dispatch({
+					type: 'setLoggedInUser',
+					data: getLoggedInUser()
+				});
+			})
+			.catch((error) => {
+				console.log(
+					'Received an error while trying to check authenticated user:',
+					error
+				);
+				setLoggedInUser(null);
+				dispatch({
+					type: 'setLoggedInUser',
+					data: null
+				});
+			});
+		// return a function that specifies any actions on component unmount
+		return () => {};
 	}, []);
 
 	return (
@@ -46,17 +72,30 @@ const App = () => {
 				<BrowserRouter>
 					<AppNavBar branding='Naruto Run' />
 					<div className='container'>
-						<Switch>
-							<Route exact path='/' component={Landing}></Route>
-							<Route exact path='/posts' component={Posts}></Route>
-							<Route exact path='/about' component={About}></Route>
-							<Route exact path='/posts/new' component={NewPost} />
-							<Route exact path='/posts/:id' component={ViewPost}></Route>
-							<Route exact path='/posts/:id/edit' component={EditPost} />
-							<Route exact path='/login' component={Login}></Route>
-							<Route exact path='/register' component={Register}></Route>
-							<Route component={NotFound}></Route>
-						</Switch>
+						{error ? (
+							<NotFound />
+						) : (
+							<Switch>
+								<Route exact path='/' component={Landing}></Route>
+								<Route exact path='/posts' component={Posts}></Route>
+								<Route exact path='/about' component={About}></Route>
+								<Route exact path='/posts/new' component={NewPost} />
+								<Route
+									exact
+									path='/posts/:id'
+									render={(props) => (
+										<NarutoPost
+											{...props}
+											post={getPostById(narutoPosts, props.match.params.id)}
+											showControls
+										/>
+									)}
+								/>
+								<Route exact path='/posts/edit/:id' component={EditPost} />
+								<Route exact path='/login' component={Login}></Route>
+								<Route exact path='/register' component={Register}></Route>
+							</Switch>
+						)}
 					</div>
 				</BrowserRouter>
 			</div>
--- naruto_run_client/src/components/posts/Post.js
+++ naruto_run_client/src/components/posts/Post.js
@@ -10,7 +10,7 @@ const NarutoPost = ({ history, post, showControls }) => {
 	const [errorMessage, setErrorMessage] = useState(null);
 	// If we don't have a post, return null
 	if (!post) return null;
-
+	console.log('Show Controls is showing: ', showControls);
 	const {
 		username,
 		// modified_date,
@@ -29,12 +29,13 @@ const NarutoPost = ({ history, post, showControls }) => {
 	} = post;
 
 	const allowEditDelete = loggedInUser && loggedInUser === post.username;
+	console.log('allow is showing: ', allowEditDelete);
 	// Handle the delete button
 	function handleDelete(e) {
 		e.preventDefault();
 		deleteNarutoPost(post._id)
 			.then(() => {
-				console.log('deleted post');
+				console.log('Deleted post');
 				const updatedPosts = narutoPosts.filter(
 					(narutoPost) => narutoPost._id !== post._id
 				);
@@ -46,7 +47,7 @@ const NarutoPost = ({ history, post, showControls }) => {
 			})
 			.catch((error) => {
 				const status = error.response ? error.response.status : 500;
-				console.log('caught error on edit', error);
+				console.log('Caught an error on edit', error);
 				if (status === 403)
 					setErrorMessage(
 						'Oops! It appears we lost your login session. Make sure 3rd party cookies are not blocked by your browser settings.'
@@ -103,105 +104,110 @@ const NarutoPost = ({ history, post, showControls }) => {
 										value={current_tech_job}
 									/>
 								</div>
-								<div className='form-group'>
-									<label htmlFor='education'>Education:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='education'
-										value={education}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='resources_required'>
-										Resources Required:
-									</label>
-									<input
-										type='text'
-										className='form-control'
-										name='resources_required'
-										value={resources_required}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='time_taken'>Time Taken:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='time_taken'
-										value={time_taken}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='cost'>Cost:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='cost'
-										value={cost}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='journey'>Your journey:</label>
-									<textarea
-										type='text'
-										className='form-control'
-										name='journey'
-										rows='3'
-										value={journey}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='tech_stack'>Top 3 Techs:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='tech_stack'
-										value={tech_stack}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='os_allegiance'>OS Allegiance:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='os_allegiance'
-										value={os_allegiance}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='fueled_by'>Fueled by:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='fueled_by'
-										value={fueled_by}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='favourite_coding_playlist'>
-										Favourite Coding Playlist:
-									</label>
-									<input
-										type='text'
-										className='form-control'
-										name='favourite_coding_playlist'
-										value={favourite_coding_playlist}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='follow_me_links'>Social Links:</label>
-									<input
-										type='text'
-										className='form-control'
-										name='follow_me_links'
-										value={follow_me_links}
-									/>
-								</div>
+								{showControls && (
+									<div>
+										<div className='form-group'>
+											<label htmlFor='education'>Education:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='education'
+												value={education}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='resources_required'>
+												Resources Required:
+											</label>
+											<input
+												type='text'
+												className='form-control'
+												name='resources_required'
+												value={resources_required}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='time_taken'>Time Taken:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='time_taken'
+												value={time_taken}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='cost'>Cost:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='cost'
+												value={cost}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='journey'>Your journey:</label>
+											<textarea
+												type='text'
+												className='form-control'
+												name='journey'
+												rows='3'
+												value={journey}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='tech_stack'>Top 3 Techs:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='tech_stack'
+												value={tech_stack}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='os_allegiance'>OS Allegiance:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='os_allegiance'
+												value={os_allegiance}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='fueled_by'>Fueled by:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='fueled_by'
+												value={fueled_by}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='favourite_coding_playlist'>
+												Favourite Coding Playlist:
+											</label>
+											<input
+												type='text'
+												className='form-control'
+												name='favourite_coding_playlist'
+												value={favourite_coding_playlist}
+											/>
+										</div>
+										<div className='form-group'>
+											<label htmlFor='follow_me_links'>Social Links:</label>
+											<input
+												type='text'
+												className='form-control'
+												name='follow_me_links'
+												value={follow_me_links}
+											/>
+										</div>
+									</div>
+								)}
 							</form>
 						</div>
 					</div>
 				</div>
+				<br />
 			</div>
 			{showControls && allowEditDelete && (
 				<div>
